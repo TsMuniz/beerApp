1a69dedc83e8914b35fbcac214f1fb335c3828d3
thiago <ts_muniz@outlook.com>
2022-10-22 11:41:13 -0300

    feat: adiciona banco de dados  remote na aplicacao

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,7 +1,9 @@
 require('dotenv/config');
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
+console.log(port);
 const app = require('./app');
+
 
 app.listen(port);
 console.log(`Api rodando na porta ${port}`);
--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,22 +1,38 @@
+// require('dotenv/config')
+
+// const options = {
+//   host: process.env.PGHOST,
+//   port: Number(process.env.PGPORT),
+//   database: process.env.PGDATABASE,    
+//   username: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   dialect: 'postgres',
+//   define: {
+//     timestamps: true
+//   },
+//   logging: false,
+// };
+
+// module.exports = {
+//   development: {
+//     ...options,
+//   },
+//   test: {
+//     ...options,
+//   },
+//   production: {
+//     ...options,
+//   },
+// };
+
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
-
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
-
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database:
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'root',
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
